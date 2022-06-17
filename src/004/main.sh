299e9a5da81c62afd6e21ecb1fd70dee1a7b0534
ICanOnlySuffer <pierorojas@tutamail.com>
2022-06-17 15:48:28 -0500

    added shell version

---
+++ src/004/main.sh
@@ -0,0 +1,36 @@
+
+awk '
+BEGIN {
+	MAX=999
+	MIN=100
+	
+	for (i = MAX; i >= MIN; i--) {
+		for (j = MAX; j >= MIN; j--) {
+			number = i * j
+			
+			if (number < largest) {
+				break
+			}
+			if (is_palindrome(number)) {
+				largest = number
+				break
+			}
+		}
+	}
+	
+	print largest
+}
+
+function is_palindrome (number) {
+	original = number
+	reversed = 0
+	
+	while (number) {
+		reversed = reversed * 10 + number % 10
+		number = int (number / 10)
+	}
+	
+	return original == reversed
+}
+'
+
